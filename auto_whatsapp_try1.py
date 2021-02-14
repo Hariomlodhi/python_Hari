bfd04dacac2565e4ce7cfa7b1f2265977b420713
Hariomlodhi <79036629+Hariomlodhi@users.noreply.github.com>
2021-02-14 12:19:29 +0530

    Update auto_whatsapp_try1.py

--- auto_whatsapp_try1.py
+++ auto_whatsapp_try1.py
@@ -2,7 +2,7 @@ import pywhatkit as kit
 from pynput.keyboard import Key, Controller
 #kit.add_driver_path('chromedriver.exe')
 #kit.load_QRcode()
-kit.sendwhatmsg('+91mobile number', 'you`s message', 21,6)
+kit.sendwhatmsg('+91mobile number', 'your`s message', 21,6)
 keyboard = Controller()
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
